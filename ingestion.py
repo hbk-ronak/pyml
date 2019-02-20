2b64b1effb58ace7c9141d7eba7391dbcbac01be
FACTS-MAC-001 <facts-mac-001@Nikhils-MacBook-Pro-3.local>
2019-02-20 22:31:06 +0530

    created data ingestion module to read data from a zip file

---
+++ ingestion.py
@@ -0,0 +1,17 @@
+import zipfile as zp
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+def ingest(path):
+    '''
+    Loads data from a zip. The Zip should contain one train.csv file
+    '''
+    files = [f for f in listdir(path) if isfile(join(path, f)) if '.zip' in f]
+    with zp.ZipFile(path+files[0]) as z:
+        with z.open("train.csv") as f:
+            df = pd.read_csv(f)
+    n_rows, n_cols = df.shape
+    logs = "The File contains: %i rows and %i columns" %(n_rows,n_cols)
+    print(logs)
+    return df
